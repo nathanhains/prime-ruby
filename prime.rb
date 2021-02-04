f407819ed9182669e77ba618efe169cd0941df9e
nathan hains <nathanhains14@yahoo.com>
2021-02-04 00:39:58 -0500

    commit tree ecc684ccadb8c6dd5546e255d963b4059fd99a41

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+    if integer <= 1
+        return false
+      else 
+          if (2...integer).any? { |i| integer % i  == 0}
+              return false
+          else
+              return true
+          end 
+      end
+  end
